8a849538f7d9c0ddafea4f4ed099a92bb8af2c9b
Marc Gallardo Ruiz <marc.gallardo@inqbarna.com>
2020-03-30 23:07:58 +0200

    Set correctly uitableviewcontroller

---
+++ MarvelWikiApp/MainScreen/Controllers/MainViewController.swift
@@ -0,0 +1,60 @@
+//
+//  MainViewController.swift
+//  MarvelWikiApp
+//
+//  Created by Marc Gallardo on 22/03/2020.
+//  Copyright © 2020 Marc Gallardo. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+
+    
+    @IBOutlet weak var tableViewHeroes: UITableView!
+    
+    private let marvelClient = MarvelHeroeService()
+    var heroesCharacter : [MarvelHeroe] = []
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        downloadCharacters()
+        tableViewHeroes.delegate = self
+        tableViewHeroes.dataSource = self
+        tableViewHeroes.register(UINib(nibName: "MarvelHeroMainTableViewCell", bundle: nil), forCellReuseIdentifier: "MarvelHeroeCell")
+    }
+    
+    func downloadCharacters() {
+          marvelClient.callAPICharacters { (heroes, error) in
+              if (error == nil){
+                self.heroesCharacter = heroes
+                self.tableViewHeroes.reloadData()
+              }
+          }
+      }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return heroesCharacter.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableViewHeroes.dequeueReusableCell(withIdentifier: "MarvelHeroeCell", for: indexPath) as! MarvelHeroMainTableViewCell
+
+        if heroesCharacter.count == 1 {
+            cell.lblNameHeroe.text = "No hay datos"
+        } else {
+            let heroeMarvelDetail = heroesCharacter[indexPath.row]
+            cell.lblNameHeroe.text = heroeMarvelDetail.name
+            cell.descripHeroe.text = heroeMarvelDetail.descrip
+            cell.lblNumSeries.text = String(describing: heroeMarvelDetail.numSeries)
+            cell.lblNumComics.text = String(describing: heroeMarvelDetail.numComic)
+            return cell
+        }
+                return cell
+            }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 250.0
+    }
+}
+
