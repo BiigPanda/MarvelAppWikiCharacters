995eff34df06336156c0f3a8e730c72e365021c6
Marc Gallardo Ruiz <marc.gallardo@inqbarna.com>
2020-04-01 14:22:50 +0200

    Set search bar in main screen

--- MarvelWikiApp/MainScreen/Controllers/MainViewController.swift
+++ MarvelWikiApp/MainScreen/Controllers/MainViewController.swift
@@ -10,19 +10,22 @@ import UIKit
 import JGProgressHUD
 import SDWebImage
 
-class MainViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
-
+class MainViewController: UIViewController,UITableViewDelegate,UITableViewDataSource,UISearchBarDelegate {
+    @IBOutlet weak var searchBarHeroe: UISearchBar!
     
     @IBOutlet weak var tableViewHeroes: UITableView!
     
     private let marvelClient = MarvelHeroeService()
     var heroesCharacter : [MarvelHeroe] = []
+    var filterHeroesCharacter : [MarvelHeroe] = []
         
     override func viewDidLoad() {
         super.viewDidLoad()
         downloadCharacters()
         tableViewHeroes.delegate = self
         tableViewHeroes.dataSource = self
+        tableViewHeroes.keyboardDismissMode = .onDrag
+        searchBarHeroe.placeholder = "Search by Name of Heroe..."
         tableViewHeroes.register(UINib(nibName: "MarvelHeroMainTableViewCell", bundle: nil), forCellReuseIdentifier: "MarvelHeroeCell")
     }
     
@@ -40,14 +43,27 @@ class MainViewController: UIViewController,UITableViewDelegate,UITableViewDataSo
       }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if filterHeroesCharacter.count > 0 {
+            return filterHeroesCharacter.count
+        }
         return heroesCharacter.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableViewHeroes.dequeueReusableCell(withIdentifier: "MarvelHeroeCell", for: indexPath) as! MarvelHeroMainTableViewCell
 
-        if heroesCharacter.count == 0 {
-            cell.lblNameHeroe.text = "No hay datos"
+        if filterHeroesCharacter.count > 0 {
+            let heroeMarvelDetail = filterHeroesCharacter[indexPath.row]
+            cell.lblNameHeroe.text = heroeMarvelDetail.name
+            if heroeMarvelDetail.descrip == "" || heroeMarvelDetail.descrip == nil {
+                cell.descripHeroe.text = "This heroe hasn't description"
+            } else {
+                cell.descripHeroe.text = heroeMarvelDetail.descrip
+            }
+            cell.lblNumSeries.text = "\(heroeMarvelDetail.numSeries ?? 0)"
+            cell.lblNumComics.text = "\(heroeMarvelDetail.numComic ?? 0)"
+            cell.imgHeroe.sd_setImage(with: URL(string: heroeMarvelDetail.thumbnail), placeholderImage: UIImage(named:"img_splash_logo"))
+            return cell
         } else {
             let heroeMarvelDetail = heroesCharacter[indexPath.row]
             cell.lblNameHeroe.text = heroeMarvelDetail.name
@@ -61,11 +77,28 @@ class MainViewController: UIViewController,UITableViewDelegate,UITableViewDataSo
             cell.imgHeroe.sd_setImage(with: URL(string: heroeMarvelDetail.thumbnail), placeholderImage: UIImage(named:"img_splash_logo"))
             return cell
         }
-                return cell
-            }
+    }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 250.0
     }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        guard !searchText.isEmpty else {
+            filterHeroesCharacter = heroesCharacter
+            tableViewHeroes.reloadData()
+            return
+        }
+        filterHeroesCharacter = heroesCharacter.filter({ (heroe) -> Bool in
+            guard let text = searchBar.text else {return false}
+            return heroe.name.contains(text)
+        })
+        tableViewHeroes.reloadData()
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBarHeroe.resignFirstResponder()
+    }
+    
 }
 
